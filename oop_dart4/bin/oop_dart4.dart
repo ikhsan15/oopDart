e8b9fb4530dac7915e425e708c9b736f5a4b06e2
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-02-05 15:12:51 +0700

    bagian 4 - default & optional parameter

--- oop_dart4/bin/oop_dart4.dart
+++ oop_dart4/bin/oop_dart4.dart
@@ -1,27 +1,44 @@
 import 'dart:io';
 
-double luas_segiempat(double panjang, double lebar) {
-  double hasil;
-  hasil = panjang * lebar;
-  return hasil;
-}
-
 // tidak perlu pengembalian nilai
 // void sapa_viewer() {
 //   print("hello guys...");
 // }
+
+// double luas_segiempat(double panjang, double lebar) {
+//   double hasil;
+//   hasil = panjang * lebar;
+//   return hasil;
+// }
+
+// default parameter
+// ... {String to, String appname = "ig"}) {
+
+// optional parameter
+String say(String from, String message, {String to, String appname}) {
+  return from +
+      " say " +
+      message +
+      ((to != null) ? "to " + to : "") +
+      ((appname != null) ? "via " + appname : "");
+}
 
 main(List<String> arguments) {
   // void
   // sapa_viewer();
 
   // mengembalikan nilai
-  double p, l, luas;
-  print("masukkan nilai panjang");
-  p = double.tryParse(stdin.readLineSync());
-  print("masukkan nilai lebar");
-  l = double.tryParse(stdin.readLineSync());
+  // double p, l, luas;
+  // print("masukkan nilai panjang");
+  // p = double.tryParse(stdin.readLineSync());
+  // print("masukkan nilai lebar");
+  // l = double.tryParse(stdin.readLineSync());
 
-  luas = luas_segiempat(p, l);
-  print(luas);
+  // luas = luas_segiempat(p, l);
+  // print(luas);
+
+  // optional parameter
+  print(say("ikhsan", "hello "));
+  print(say("ikhsan", "hello ", appname: "telegram"));
+  print(say("ikhsan", "hello ", to: "nur ", appname: "telegram"));
 }
