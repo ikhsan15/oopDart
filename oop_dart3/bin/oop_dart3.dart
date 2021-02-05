abd609314142d9e9f245c9f3955fcf7daa9ba87a
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-02-05 10:44:30 +0700

    bagian 3 - perulangan increment & decrement

--- oop_dart3/bin/oop_dart3.dart
+++ oop_dart3/bin/oop_dart3.dart
@@ -1,12 +1,23 @@
 import 'dart:io';
 
 main(List<String> arguments) {
+  // increment & decrement
+  int a, b;
+  a = 10;
+  b = a++;
+  print(a.toString() + " - " + b.toString());
+
+  int x, y;
+  x = 10;
+  y = ++x;
+  print(x.toString() + " - " + y.toString());
+
   // perulangan dengan do while
-  int x = 0;
-  do {
-    print("halo ke " + x.toString());
-    x++;
-  } while (x < 5);
+  // int x = 0;
+  // do {
+  //   print("halo ke " + x.toString());
+  //   x++;
+  // } while (x < 5);
 
   // perulangan dengan while
   // int i = 0;
