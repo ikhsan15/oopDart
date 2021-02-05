b4cb471a3eb9c03e3130ff65a62b636a9bd6864f
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-02-05 10:40:14 +0700

    bagian 3 - perulangan dengan while dan do while

--- oop_dart3/bin/oop_dart3.dart
+++ oop_dart3/bin/oop_dart3.dart
@@ -1,7 +1,22 @@
 import 'dart:io';
 
 main(List<String> arguments) {
-  for (int counter = 0; counter < 5; counter++) {
-    print("halo");
-  }
+  // perulangan dengan do while
+  int x = 0;
+  do {
+    print("halo ke " + x.toString());
+    x++;
+  } while (x < 5);
+
+  // perulangan dengan while
+  // int i = 0;
+  // while (i < 5) {
+  //   print("halo ke " + i.toString());
+  //   i++;
+  // }
+
+  // perulangan dengan for
+  // for (int counter = 0; counter < 5; counter++) {
+  //   print("halo");
+  // }
 }
