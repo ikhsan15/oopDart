0b02905107369d99e390a538fc630c53daf70fad
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-02-05 09:34:34 +0700

    bagian 2 - percabangan dengan switch

--- oop_dart2/bin/oop_dart2.dart
+++ oop_dart2/bin/oop_dart2.dart
@@ -3,11 +3,27 @@ import 'dart:io';
 main(List<String> args) {
   int number = int.tryParse(stdin.readLineSync());
 
-  if (number > 0) {
-    print("positif");
-  } else if (number < 0) {
-    print("negatif");
-  } else {
-    print("nol");
+  // jika var bernilai tetap bisa menggunakan switch
+  switch (number) {
+    case 0:
+      print("nol");
+      break;
+    case 1:
+      print(number);
+      print("satu");
+      break;
+    case 2:
+      print("dua");
+      break;
+    default:
+      print("bilangan lain");
   }
+
+  // if (number > 0) {
+  //   print("positif");
+  // } else if (number < 0) {
+  //   print("negatif");
+  // } else {
+  //   print("nol");
+  // }
 }
