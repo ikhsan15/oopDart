7fbd56e5157096c70a23e89a8855f36069fd58b8
Ikhsan Nur <ikhsan15nur@gmail.com>
2021-02-05 14:20:31 +0700

    bagian 4 - function dengan void

--- oop_dart4/bin/oop_dart4.dart
+++ oop_dart4/bin/oop_dart4.dart
@@ -6,9 +6,17 @@ double luas_segiempat(double panjang, double lebar) {
   return hasil;
 }
 
-main(List<String> arguments) {
-  double p, l, luas;
+// tidak perlu pengembalian nilai
+// void sapa_viewer() {
+//   print("hello guys...");
+// }
 
+main(List<String> arguments) {
+  // void
+  // sapa_viewer();
+
+  // mengembalikan nilai
+  double p, l, luas;
   print("masukkan nilai panjang");
   p = double.tryParse(stdin.readLineSync());
   print("masukkan nilai lebar");
